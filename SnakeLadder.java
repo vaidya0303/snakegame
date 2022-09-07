c6b2fa49e52c7652b76a1601b3f6234490a91eae
saurabh vaidya <vaidya0304@gmail.com>
2022-09-07 20:09:15 +0530

    branch2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,16 +1,24 @@
-class Player{
-    int current_position = 0;	
-    //Snake and Ladder game played with single player at start position 0
-    }
-    
+//Snake and Ladder game played with single player at start position 0
+class Player {
+	int current_position = 0;
+	Player(){}
+	int rollADie() {
+		int dieOutCome = (int)(Math.floor(Math.random() *10)%6)+1;
+		return dieOutCome;
+
+	} 
+
+	//The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number
+
     public class SnakeLadder {
+	
         public static void main( String[] args )
-    
         {
             Player P1= new Player();
-            System.out.println(P1.current_position);
-    
-          }
-           
-           
+            int result = P1.rollADie();	
+
+		System.out.println(P1.current_position);  
+		System.out.println(result);
+	   }
     }
+}
