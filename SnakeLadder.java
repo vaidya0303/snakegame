3e14b7a2bb57d8b31d223bc8632fbc9acdb7852c
saurabh vaidya <vaidya0304@gmail.com>
2022-09-07 20:22:39 +0530

    branch4th

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -14,14 +14,13 @@ class Player{
 	
 	//The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number
 	// checks for a Option that players are No Play,Ladder or snake
-	//Repeat till the Player reaches the winning position 100. 
-	//Ensure the player gets to exact winning position 100. 
+	//Repeat till the Player reaches the winning position 100. In case the player
 }
 public class SnakeLadder {
 	public static void main( String[] args )
 	{
 		Player P1= new Player();
-		while(P1.current_position != 100) {
+		while(P1.current_position < 100) {
 			int result = P1.rollADie();	
 			int check = P1.checkOption();
 			switch (check) {
@@ -29,21 +28,21 @@ public class SnakeLadder {
 				break;
 			case 1:
 				int status = P1.current_position + result;
-					P1.current_position = status;
+				P1.current_position = status;
 				break;
 			case 2:
 				int status1 = P1.current_position - result;
 				P1.current_position = status1;
 				if(P1.current_position<0)
-					P1.current_position=0;
+				P1.current_position=0;
 				break;				
 			}
-			System.out.println(result);
 			System.out.println(check);
-			System.out.println(P1.current_position);
+			System.out.println(result);
+			System.out.println(P1.current_position);  
 			
-		
+			
+			System.out.println("");
 		}
-		
-	}
+	 }
 }
