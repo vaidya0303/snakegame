eb9de117eb9016f44fef607d4071eaa6686ca3e2
saurabh vaidya <vaidya0304@gmail.com>
2022-09-07 20:27:29 +0530

    branch7th

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -16,37 +16,61 @@ class Player{
 	// checks for a Option that players are No Play,Ladder or snake
 	//Repeat till the Player reaches the winning position 100. 
 	//Ensure the player gets to exact winning position 100. 
+	//Play the game with 2 Player
 }
 public class SnakeLadder {
 	public static void main( String[] args )
 	{
 		Player P1= new Player();
+		Player P2 = new Player();
 		int total = 0;
-		while(P1.current_position != 100) {
-			int result = P1.rollADie();	
-			int check = P1.checkOption();
+		while(P1.current_position != 100 && P2.current_position !=100) {
+			int result_p1 = P1.rollADie();
+			int result_p2 = P2.rollADie();
+			int check_p1 = P1.checkOption();
+			int check_p2 = P2.checkOption();
 			total++;
-			switch (check) {
+			switch (check_p1) {
 			case 0:
 				break;
 			case 1:
-				int status = P1.current_position + result;
+				int status = P1.current_position + result_p1;
+				if(status <=100)
 					P1.current_position = status;
+					
 				break;
 			case 2:
-				int status1 = P1.current_position - result;
+				int status1 = P1.current_position - result_p1;
 				P1.current_position = status1;
 				if(P1.current_position<0)
-					P1.current_position=0;
+					P1.current_position=0; 
 				break;				
 			}
-			System.out.println("Dice outcome  "+ result);
-		
-			System.out.println("Player current position is  "+P1.current_position);
+			switch (check_p2) {
+			case 0:
+				break;
+			case 1:
+				int status = P2.current_position + result_p2;
+				if(status <=100)
+					P2.current_position = status;
+					
+				break;
+			case 2:
+				int status1 = P2.current_position - result_p2;
+				P2.current_position = status1;
+				if(P2.current_position<0)
+					P2.current_position=0; 
+				break;				
+			}
 			
-		
+			System.out.println("Player1 current position is  "+P1.current_position);
+			System.out.println("Player2 current position is  "+P2.current_position);
 		}
 		System.out.println("Number of times the dice was played to win  "+ total);
+		if(P1.current_position >= 100)
+			System.out.println("Player1 won the game");
+		else
+			System.out.println("Player2 won the game");
 		
-	 }
+	}
 }
