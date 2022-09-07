6dafe1288a1a20afa9383916e5d70a55226b7ea9
saurabh vaidya <vaidya0304@gmail.com>
2022-09-07 20:02:03 +0530

    first

---
+++ SnakeLadder.java
@@ -0,0 +1,16 @@
+class Player{
+    int current_position = 0;	
+    //Snake and Ladder game played with single player at start position 0
+    }
+    
+    public class SnakeLadder {
+        public static void main( String[] args )
+    
+        {
+            Player P1= new Player();
+            System.out.println(P1.current_position);
+    
+        }
+           
+           
+    }
